de216256b9cb8eaff2359929cc61e7007f5371f4
GuuhX23 <gu.paulella@gmail.com>
2026-07-14 20:47:28 -0300

    Service layer, componen registration

---
+++ src/main/java/com/webservice/web/services/UserService.java
@@ -0,0 +1,26 @@
+package com.webservice.web.services;
+
+import com.webservice.web.entities.User;
+import com.webservice.web.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+}
